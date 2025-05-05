f7b84d32e9478c6fb89560b293d0945b2592995f
Lawand5 <lama0007@stud.kea.dk>
2025-05-05 16:22:27 +0200

    Tilføjer testdata med data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,8 @@ INSERT INTO tourist_attraction (name, description, city, tags) VALUES
                                                                    ('Tivoli', 'Forlystelsespark midt i KBH centrum', 'København', 'Familie,Udendørs,Koncert,Restaurant'),
                                                                    ('Eiffeltårnet', 'Smukt tårn i Paris', 'Paris', 'Udendørs,Kærlighed,Europa'),
                                                                    ('Zoo', 'Se masser af dyr i KBH', 'København', 'Familie,Udendørs,Dyr,Barn');
+
+INSERT INTO tourist_attractions (name, description, city, tags)
+VALUES
+    ('Tivoli', 'En klassisk forlystelsespark i København.', 'COPENHAGEN', 'FAMILY,ADVENTURE'),
+    ('Legoland', 'Forlystelser og oplevelser for hele familien.', 'BILLUND', 'FAMILY,KIDS');
